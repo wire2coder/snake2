fa6338d7af47419c0d706c0d0e37d7a892fdd2e0
cnnicentral2 <cnnicentral2@LAP-CNN74-162.turner.com>
2022-11-04 03:09:06 -0400

    converted into OOP, again

--- main.py
+++ main.py
@@ -13,46 +13,78 @@ from pygame.locals import *
 import os
 
 
+class Snake:
+    def __init__(self, game_window):
+        # 1. get 'game_window' from game 'class'
+        self.game_window = game_window
+
+        # 2. load the block.jpg
+        self.block = pygame.image.load("resources/block.jpg")  # of an image file
+
+        # 3. set 'starting position' for block.jpg
+        self.x_position = 100
+        self.y_position = 100
+
+    def draw_block(self):
+        self.game_window.fill((110, 110, 5))  # 'erase' previous 'block.jpg' by filling background color
+        self.game_window.blit(self.block, (self.x_position, self.y_position))  # 'draw' 'block.jpg' at new location
+
+        pygame.display.flip()  # 'display the game window'
+
+
+class Game:
+    def __init__(self):
+        # 1. make game 'window'
+        pygame.init()
+        self.surface = pygame.display.set_mode((1000, 500))
+
+        # 2. fill background color for the 'window'
+        self.surface.fill((110, 110, 5))
+
+        # 3. make a 'snake'
+        self.snake1 = Snake(self.surface)
+
+        # 4. 'draw' the snake
+        self.snake1.draw_block()
+
+        # infinite game loop
+        running = True
+        while running:
+            event = pygame.event.get()
+
+            for asdf in event:
+                if asdf.type == KEYUP:  # when the key (on keyboard) bounce up
+                    if asdf.key == K_ESCAPE:
+                        running = False
+                    if asdf.key == K_UP:
+                        # subtract y value by 10
+                        self.snake1.y_position -= 10  # decrease y-axis by 10
+                        self.snake1.draw_block()
+                    if asdf.key == K_DOWN:
+                        # increase y value by 10
+                        self.snake1.y_position += 10
+                        self.snake1.draw_block()
+                    if asdf.key == K_LEFT:
+                        # subtract X value by 10
+                        self.snake1.x_position -= 10
+                        self.snake1.draw_block()
+                    if asdf.key == K_RIGHT:
+                        # increase X value by 10
+                        self.snake1.x_position += 10
+                        self.snake1.draw_block()
+
+                if asdf.type == QUIT:
+                    running = False
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    pygame.init()
-    surface = pygame.display.set_mode((1000, 500)) # making a 'window'
-    surface.fill((110, 110, 5)) # fill the window with background color
-
-    block = pygame.image.load("resources/block.jpg") # of an image file
-    x_position = 100
-    y_position = 100
-    surface.blit(block, (x_position, y_position))
+    gameWindow1 = Game()
 
 
-    pygame.display.flip() # reverse the x and y axis
-
-    # infinite game loop
-    running = True
-    while running:
-        event = pygame.event.get()
-
-        for asdf in event:
-            if asdf.type == KEYUP: # when the key (on keyboard) bounce up
-                if asdf.key == K_ESCAPE:
-                    running = False
-                if asdf.key == K_UP:
-                    # subtract y value by 10
-                    surface.fill((110, 110, 5)) # 'erase' previous 'block.jpg' by filling background color
-                    y_position -= 10 # decrease y-axis by 10
-                    surface.blit(block, (x_position, y_position)) # 'draw' 'block.jpg' at new location
-                    pygame.display.flip() # 'display the game window'
-                if asdf.key == K_DOWN:
-                    # increase y value by 10
-                    y_position += 10
-                    surface.blit(block, (x_position, y_position))
-                    pygame.display.flip()
-                # if asdf.key == K_LEFT:
-                #     # subtract X value by 10
-                # if asdf.key == K_RIGHT:
-                #     # increase X value by 10
 
 
-            if asdf.type == QUIT:
-                running = False
+
+
+
